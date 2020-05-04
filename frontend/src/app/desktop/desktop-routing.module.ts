34476a4802cdd6fea208f0431f95bf58e04a7111
Tadej321 <tedy.kosovel@gmail.com>
2020-05-04 20:34:34 +0200

    Set up route guard for desktop route and its children.

--- frontend/src/app/desktop/desktop-routing.module.ts
+++ frontend/src/app/desktop/desktop-routing.module.ts
@@ -1,14 +1,15 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {DesktopComponent} from "./desktop.component";
-import {TaskListComponent} from "./task-list/task-list.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import {DesktopComponent} from './desktop.component';
+import {TaskListComponent} from './task-list/task-list.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AuthGuard} from '../authentication/auth-guard';
 
 const routes: Routes = [
   {path: 'desktop', component: DesktopComponent, children: [
-      {path: 'tasks', component: TaskListComponent},
-      {path: 'dashboard', component: DashboardComponent}
-    ]}
+      {path: 'tasks', component: TaskListComponent, canLoad: [AuthGuard]},
+      {path: 'dashboard', component: DashboardComponent, canLoad: [AuthGuard]}
+    ], canLoad: [AuthGuard]}
 ];
 
 @NgModule({
